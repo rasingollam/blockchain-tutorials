22a3b7dcd0fb393c562c670d1ce56736e13273e9
Malinda Rasingolla <rasingollam@gmail.com>
2026-06-23 14:27:10 +0530

    fix: update token address and enhance balance logging in wallet system script

--- erc20-wallet-system/scripts/wallet-system.ts
+++ erc20-wallet-system/scripts/wallet-system.ts
@@ -11,19 +11,23 @@ async function main() {
   console.log("User3:", user3.account.address);
 
   // Step B — Connect token
-  const tokenAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512" as const;
+  const tokenAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3" as const;
   const token = (await viem.getContractAt("MyToken", tokenAddress)) as any;
 
   // Step C — Balance check (LOGIN dashboard)
   console.log("User1 balance:", (await token.read.balanceOf([user1.account.address])).toString());
+  console.log("User2 balance:", (await token.read.balanceOf([user2.account.address])).toString());
+  console.log("User3 balance:", (await token.read.balanceOf([user3.account.address])).toString());
 
   // Step D — Transfer system (user1 sends 1000 tokens to user2)
+  console.log("Transferring 1000 tokens from User1 to User2...");
   const txHash = await token.write.transfer([user2.account.address, 1000n]);
   console.log("Transfer tx:", txHash);
 
-  // Step E — After transfer
+//   // Step E — After transfer
   console.log("User1:", (await token.read.balanceOf([user1.account.address])).toString());
   console.log("User2:", (await token.read.balanceOf([user2.account.address])).toString());
+  console.log("User3:", (await token.read.balanceOf([user3.account.address])).toString());
 }
 
 main().catch(console.error);
